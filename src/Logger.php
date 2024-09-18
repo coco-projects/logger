45ed42ff277b3e497fa10bcc8bc81c5f6b86d76b
coco-projects <coco-project@protonmail.com>
2024-09-18 21:22:12 +0800

    update

--- src/Logger.php
+++ src/Logger.php
@@ -5,88 +5,101 @@
     use Monolog\Handler\HandlerInterface;
     use Monolog\Handler\StreamHandler;
     use Psr\Log\LoggerInterface;
+    use Monolog\Handler\RedisHandler;
 
-    trait Logger
+trait Logger
+{
+    protected ?LoggerInterface $logger = null;
+
+    public function setLogger(?LoggerInterface $logger): void
     {
-        protected ?LoggerInterface $logger = null;
-
-        public function setLogger(?LoggerInterface $logger): void
-        {
-            $this->logger = $logger;
-        }
-
-        public function addFileLogger(string $path): void
-        {
-            $handler = new StreamHandler($path, \Monolog\Logger::DEBUG);
-            $this->pushLoggerHandler($handler);
-        }
-
-        public function addStdoutLogger(): void
-        {
-            $handler = new StreamHandler('php://stdout', \Monolog\Logger::DEBUG);
-            $this->pushLoggerHandler($handler);
-        }
-
-        public function pushLoggerHandler(HandlerInterface $handler): static
-        {
-            $this->logger && $this->logger->pushHandler($handler);
-
-            return $this;
-        }
-
-        public function logError(string $msg): static
-        {
-            $this->writeLog('error', $msg);
-
-            return $this;
-        }
-
-        public function logAlert(string $msg): static
-        {
-            $this->writeLog('alert', $msg);
-
-            return $this;
-        }
-
-        public function logInfo(string $msg): static
-        {
-            $this->writeLog('info', $msg);
-
-            return $this;
-        }
-
-        public function logDebug(string $msg): static
-        {
-            $this->writeLog('debug', $msg);
-
-            return $this;
-        }
-
-        public function logEmergency(string $msg): static
-        {
-            $this->writeLog('emergency', $msg);
-
-            return $this;
-        }
-
-        public function logNotice(string $msg): static
-        {
-            $this->writeLog('notice', $msg);
-
-            return $this;
-        }
-
-        public function logWarning(string $msg): static
-        {
-            $this->writeLog('warning', $msg);
-
-            return $this;
-        }
-
-        private function writeLog(string $level, string $msg): void
-        {
-            $this->logger && $this->logger->{$level}($msg);
-        }
-
-
+        $this->logger = $logger;
     }
+
+    public function addRedisLogger(string $redisHost, int $redisPort, string $password = '', int $db = 10): void
+    {
+        $redis = new \Redis();
+        $redis->connect($redisHost, $redisPort);
+
+        if ($password) {
+            $redis->auth($password);
+        }
+
+        $redis->select($db);
+        $handler = new RedisHandler($redis, 'log_channel', \Monolog\Logger::DEBUG);
+        $this->pushLoggerHandler($handler);
+    }
+
+    public function addFileLogger(string $path): void
+    {
+        $handler = new StreamHandler($path, \Monolog\Logger::DEBUG);
+        $this->pushLoggerHandler($handler);
+    }
+
+    public function addStdoutLogger(): void
+    {
+        $handler = new StreamHandler('php://stdout', \Monolog\Logger::DEBUG);
+        $this->pushLoggerHandler($handler);
+    }
+
+    public function pushLoggerHandler(HandlerInterface $handler): static
+    {
+        $this->logger && $this->logger->pushHandler($handler);
+
+        return $this;
+    }
+
+    public function logError(string $msg): static
+    {
+        $this->writeLog('error', $msg);
+
+        return $this;
+    }
+
+    public function logAlert(string $msg): static
+    {
+        $this->writeLog('alert', $msg);
+
+        return $this;
+    }
+
+    public function logInfo(string $msg): static
+    {
+        $this->writeLog('info', $msg);
+
+        return $this;
+    }
+
+    public function logDebug(string $msg): static
+    {
+        $this->writeLog('debug', $msg);
+
+        return $this;
+    }
+
+    public function logEmergency(string $msg): static
+    {
+        $this->writeLog('emergency', $msg);
+
+        return $this;
+    }
+
+    public function logNotice(string $msg): static
+    {
+        $this->writeLog('notice', $msg);
+
+        return $this;
+    }
+
+    public function logWarning(string $msg): static
+    {
+        $this->writeLog('warning', $msg);
+
+        return $this;
+    }
+
+    private function writeLog(string $level, string $msg): void
+    {
+        $this->logger && $this->logger->{$level}($msg);
+    }
+}
